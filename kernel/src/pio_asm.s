cc2024125d6b03ac460aa2c017f4b8367f4d820c
orpheeantoniadis <orphee.antoniadis@gmail.com>
2018-06-25 18:11:10 +0200

    Report files restructuration. Write about ports and start writing about interrupts

--- kernel/src/pio_asm.s
+++ kernel/src/pio_asm.s
@@ -6,45 +6,45 @@ global inw
 section .txt
 
 outb:
-  push ebp
-  mov ebp, esp
+    push ebp
+    mov ebp, esp
 
-	mov word dx, [esp+8]
-	mov byte al, [esp+12]
-	out dx, al
-  
-  mov eax, 0
-  leave
-	ret
+    mov word dx, [esp+8]
+    mov byte al, [esp+12]
+    out dx, al
+
+    mov eax, 0
+    leave
+    ret
 
 outw:
-  push ebp
-  mov ebp, esp
-  
-	mov word dx, [esp+8]
-	mov word ax, [esp+12]
-	out dx, ax
-  
-  mov eax, 0
-  leave
-	ret
+    push ebp
+    mov ebp, esp
+
+    mov word dx, [esp+8]
+    mov word ax, [esp+12]
+    out dx, ax
+
+    mov eax, 0
+    leave
+    ret
 
 inb:
-  push ebp
-  mov ebp, esp
-  
-	mov word dx, [esp+8]
-	in byte al, dx
-  
-  leave
-	ret
+    push ebp
+    mov ebp, esp
+
+    mov word dx, [esp+8]
+    in byte al, dx
+
+    leave
+    ret
 
 inw:
-  push ebp
-  mov ebp, esp
+    push ebp
+    mov ebp, esp
 
-	mov word dx, [esp+8]
-	in word ax, dx
-  
-  leave
-	ret
+    mov word dx, [esp+8]
+    in word ax, dx
+
+    leave
+    ret
